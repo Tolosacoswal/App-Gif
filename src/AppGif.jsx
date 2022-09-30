f098fe4d3be1576a234d8a379797d7e6b3a865c5
Tolosacoswal <qs.wal123@gmail.com>
2022-09-30 16:22:07 -0500

    30-09-22-viernes

--- src/AppGif.jsx
+++ src/AppGif.jsx
@@ -1,10 +1,24 @@
 
-
+import { useState } from "react";
 export const AppGif = () => {
-  return (
+
+const [categories, setCategories] = useState(["one punch","dragon ball","atack of titans"]);
+const onAddCategory =()=>{
+  setCategories([...categories,"valorant"]);
+}
+ 
+
+return (
     <>
     <h1>AppGif</h1>
-    <h2>ejercicio</h2>
+    <button onClick={onAddCategory}>agregar</button>
+    <ol>
+      {categories.map( category => {return <li key={category}>{category}</li>})};
+    </ol>
+    
+
+
+
     </>
     )
 }
